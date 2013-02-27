ec649112a6ba7291d5d9f3fbb42c565a70d04d9a
Tim Anema <tim@zenapsis.com>
2013-02-26 19:53:32 -0500

    clean up a bit

--- public/javascripts/src/extend.js
+++
@@ -1,7 +0,0 @@
-Function.prototype.extend = function(parent) {
-  var child = this;
-  child.prototype = parent;
-  child.prototype.$super = parent;
-  child.prototype = new child(Array.prototype.slice.call(arguments,1));
-  child.prototype.constructor = child;
-};
