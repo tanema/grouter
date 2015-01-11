deb15befa90f59d7c84c5061816c0117025e2719
Tim Anema <timanema@gmail.com>
2015-01-11 15:21:19 -0500

    just playin

--- app.go
+++ app.go
@@ -8,27 +8,18 @@ import (
   "grouter/socketio"
 )
 
-var (
-  maps = []string{"map0", "map1"}
-  port = 3001
-)
+var maps = []string{"map0", "map1"}
+const port = 3001
 
 func main() {
   sio := socketio.NewSocketIOServer(&socketio.Config{})
   sio.Handle("/", http.FileServer(http.Dir("./public/")))
-
-  sio.On("connect", func(ns *socketio.NameSpace){
-    log.Println("Connected: ", ns.Id())
-  })
-  sio.On("disconnect", func(ns *socketio.NameSpace){
-    log.Println("Disconnected: ", ns.Id())
-  })
 
   for _, map_name := range maps {
     new_map := json_map.NewMap("public/maps/"+map_name+".json", sio)
     go new_map.Run()
   }
 
-	log.Println("listening on port ", port)
+	log.Println("Listening on port ", port)
   log.Fatal(http.ListenAndServe(fmt.Sprint(":", port), sio))
 }
